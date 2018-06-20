b09a66ec7889a0af0be77280171d7912799ad746
Tung Luu <tunglt185@gmail.com>
2018-06-21 01:15:11 +0700

    add stringutil lib

--- hello.go
+++ hello.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/kid-coder/stringutil"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println(stringutil.Reverse("dlroW olleH"))
 }
